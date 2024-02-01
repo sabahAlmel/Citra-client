8e705af3500536c64b63fac1ccf022f4df3859cb
YahyaNashar22 <cenations.people@live.com>
2024-02-01 14:56:53 +0200

    asd

--- client/src/components/AddProduct/AddProduct.js
+++ client/src/components/AddProduct/AddProduct.js
@@ -1,17 +1,100 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./AddProduct.module.css";
 
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 //WHEN WE FETCH WE NEED TO APPEND BOTH "formData" AND "selectedOptions" TO THE AXIOS REQUEST !!!
+
+const MoreColorsAndSizes = ({
+  details = { color: "", size: [] },
+  setColor,
+}) => {
+  const { color, size } = details;
+
+  return (
+    <>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: " 0 20px",
+        }}
+        noValidate
+        autoComplete
+      >
+        <p>اللون</p>
+        <TextField
+          name="color"
+          id="color"
+          placeholder="color"
+          variant="outlined"
+          onChange={(e) => {
+            setColor({ ...details, color: e.target.value });
+          }}
+        />
+      </Box>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: " 0 20px",
+        }}
+        noValidate
+        autoComplete
+      >
+        <p>الحجم</p>
+        <Select
+          id="size"
+          onChange={(e) => {
+            const selectedSizes = Array.isArray(e.target.value)
+              ? e.target.value
+              : [e.target.value];
+            setColor({ ...details, size: selectedSizes });
+          }}
+          multiple
+          name="size"
+          label="size"
+          value={size}
+        >
+          <MenuItem value="حجم واحد" selected={true}>
+            الحجم
+          </MenuItem>
+          <MenuItem value={"L"}>L</MenuItem>
+          <MenuItem value={"M"}>M</MenuItem>
+          <MenuItem value={"S"}>S</MenuItem>
+          <MenuItem value={"2/3"}>2/3</MenuItem>
+          <MenuItem value={"3/4"}>3/4</MenuItem>
+          <MenuItem value={"4/5"}>4/5</MenuItem>
+          <MenuItem value={"5/6"}>5/6</MenuItem>
+          <MenuItem value={"6/7"}>6/7</MenuItem>
+          <MenuItem value={"7/8"}>7/8</MenuItem>
+          <MenuItem value={"8/9"}>8/9</MenuItem>
+        </Select>
+      </Box>
+    </>
+  );
+};
 
 function AddProduct() {
   //multi size select for one color
 
   const [selectedOptions, setSelectedOptions] = useState([]);
+  const [newData, setNewData] = useState(0);
+  const [details, setDetails] = useState({
+    color: "",
+    size: [],
+  });
 
   const handleSelectChange = (event) => {
     setSelectedOptions(event.target.value);
@@ -23,13 +106,57 @@ function AddProduct() {
     arabicName: "",
     price: "",
     serialNumber: "",
-    color: [],
-    type: "",
+    details: [{ color: "", size: [] }],
+    type: null,
     description: "",
     category: "",
-    subCategory: "",
+    subCategory: null,
     images: "",
   });
+  const [categories, setCategories] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
+
+  const getCategories = async () => {
+    try {
+      let res = await axios.get(
+        `${process.env.REACT_APP_BACKEND}category/getall`
+      );
+      setCategories(res.data.categories);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const getSubCategories = async (id) => {
+    console.log(id);
+    try {
+      let res = await axios.get(
+        `${process.env.REACT_APP_BACKEND}subCategory/getsubbycategory/${id}`
+      );
+      console.log(res.data);
+      setSubCategory(res.data.subCategories);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCategories();
+    getSubCategories();
+  }, []);
+
+  const handleAddMore = (e) => {
+    e.preventDefault();
+    if (newData === 0) {
+      setNewData(newData + 1);
+      return;
+    }
+    if (newData >= 1) {
+      setFormData({ ...formData, details: [...formData.details, details] });
+      setNewData(newData + 1);
+      details;
+    }
+  };
 
   const handleChange = (e) => {
     let { name, value } = e.target;
@@ -37,7 +164,29 @@ function AddProduct() {
       ...prevState,
       [name]: value,
     }));
-    console.log(formData);
+  };
+
+  const postProduct = async (e) => {
+    e.preventDefault();
+    if (newData === 1) {
+      setFormData({ ...formData, details: [details] });
+    } else {
+      setFormData({ ...formData, details: [...formData.details, details] });
+    }
+    await axios
+      .post(`${process.env.REACT_APP_BACKEND}product/create`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        toast.success("تمت اضافة المنتج بنجاح");
+      })
+      .catch((err) => {
+        toast.error("حصل خطأ أثناء اضافة المنج");
+        console.log(err);
+        console.log(formData);
+      });
   };
 
   return (
@@ -101,7 +250,7 @@ function AddProduct() {
         >
           <p>السعر</p>
           <TextField
-            price="price"
+            name="price"
             id="price"
             label="price"
             onChange={handleChange}
@@ -143,13 +292,13 @@ function AddProduct() {
           noValidate
           autoComplete
         >
-          <p>اللون</p>
+          <p>الوصف</p>
           <TextField
-            name="color"
-            id="color"
-            placeholder="color"
-            variant="outlined"
+            id="description"
+            name="description"
+            placeholder="description"
             onChange={handleChange}
+            variant="outlined"
             required
           />
         </Box>
@@ -190,78 +339,27 @@ function AddProduct() {
           noValidate
           autoComplete
         >
-          <p>الحجم</p>
-          <Select
-            id="size"
-            onChange={handleSelectChange}
-            multiple
-            name="size"
-            label="size"
-            value={selectedOptions}
-          >
-            <MenuItem value="" selected={true}>
-              الحجم
-            </MenuItem>
-            <MenuItem value={"L"}>L</MenuItem>
-            <MenuItem value={"M"}>M</MenuItem>
-            <MenuItem value={"S"}>S</MenuItem>
-            <MenuItem value={"2/3"}>2/3</MenuItem>
-            <MenuItem value={"3/4"}>3/4</MenuItem>
-            <MenuItem value={"4/5"}>4/5</MenuItem>
-            <MenuItem value={"5/6"}>5/6</MenuItem>
-            <MenuItem value={"6/7"}>6/7</MenuItem>
-            <MenuItem value={"7/8"}>7/8</MenuItem>
-            <MenuItem value={"8/9"}>8/9</MenuItem>
-          </Select>
-        </Box>
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "25ch" },
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            padding: " 0 20px",
-          }}
-          noValidate
-          autoComplete
-        >
-          <p>الوصف</p>
-          <TextField
-            id="description"
-            name="description"
-            placeholder="description"
-            onChange={handleChange}
-            variant="outlined"
-            required
-          />
-        </Box>
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "25ch" },
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            padding: " 0 20px",
-          }}
-          noValidate
-          autoComplete
-        >
           <p>الخانة الرئيسية</p>
           <Select
             id="category"
             label="category"
-            name="category"
-            onChange={handleChange}
+            name="categoryID"
+            onChange={(e) => {
+              setFormData({ ...formData, subCategory: null });
+              setSubCategory([]);
+              handleChange(e);
+              getSubCategories(e.target.value);
+            }}
             required
           >
             <MenuItem value="" selected={true}>
               الخانة الرئيسية
             </MenuItem>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
+            {categories.map((category) => (
+              <MenuItem key={category._id} value={category._id}>
+                {category.arabicName}
+              </MenuItem>
+            ))}
           </Select>
         </Box>
         <Box
@@ -280,15 +378,17 @@ function AddProduct() {
           <Select
             id="subCategory"
             label="subCategory"
-            name="subCategory"
+            name="subCategoryID"
             onChange={handleChange}
           >
             <MenuItem value="" selected={true}>
               الخانة الفرعية
             </MenuItem>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
+            {subCategory.map((subCat) => (
+              <MenuItem value={subCat._id} key={subCat._id}>
+                {subCat.arabicName}
+              </MenuItem>
+            ))}
           </Select>
         </Box>
 
@@ -309,17 +409,28 @@ function AddProduct() {
             type="file"
             id="files"
             onChange={handleChange}
-            name="image"
+            name="images"
             multiple
           />
         </Box>
+        {newData && (
+          <>
+            {Array.from({ length: newData }).map((_, index) => (
+              <MoreColorsAndSizes
+                key={index}
+                setColor={setDetails}
+                details={details}
+              />
+            ))}
+          </>
+        )}
       </div>
-      {/* <button
-        className={style.addColors}
-        type="button">
+      <button className={style.addColors} type="button" onClick={handleAddMore}>
         أضف ألوان أخرى
-      </button> */}
-      <button className={style.add}>أضف المنتج</button>
+      </button>
+      <button className={style.add} onClick={postProduct}>
+        أضف المنتج
+      </button>
     </form>
   );
 }
