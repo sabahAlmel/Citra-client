dd4ba11bbe63d31a070af9d79e2ff391cfd1f63f
Rayan <rayanalhassan2000@gmail.com>
2024-02-01 16:54:17 +0200

    solve conflict

--- client/src/routes/AppRoutes.js
+++ client/src/routes/AppRoutes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home.js";
+import AddProduct from "../components/AddProduct/AddProduct.js";
+import AddressForm from "../../src/components/AddressForm/AddressForm.js"
 import Products from "../pages/Dashboard/Products/Products.js";
 import Users from "../pages/Dashboard/Users/Users.js";
 import OverView from "../pages/Dashboard/OverView/OverView.js";
@@ -25,7 +27,7 @@ function AppRoutes() {
         <Route path="/cart" element={<Cart />}></Route>
         <Route path="/singleProduct" element={<SingleProduct />}></Route>
         <Route path="/forgetpassword" element={<ForgetPassword />}></Route>
-        <Route path="/orderform" element={<Address />}></Route>
+        <Route path="/orderform" element={<AddressForm />}></Route>
         <Route path="/test" element={<AddProduct />}></Route>
       </Route>
       <Route path="/signin" element={<SignIn />}></Route>
