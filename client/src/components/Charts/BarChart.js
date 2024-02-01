0868c62844f26d63bd7fd646a5c4965f7a29f4f4
Mariamsuleiman <suleimanmariam591@gmail.com>
2024-02-01 15:13:25 +0200

    merge

--- client/src/components/Charts/BarChart.js
+++ client/src/components/Charts/BarChart.js
@@ -1,83 +1,9 @@
-// // // import { Chart } from "chart.js";
-// // // import { useState } from "react";
-// // import { Bar } from "react-chartjs-2";
-// // // import {chart as ChartJS} from "chart.js/auto"
-
-
-// // function BarChart() {
-   
-// // return(
-  
-// // )
-// // }
-
-// // export default BarChart;
-
-// import React from "react";
-// import { Chart as ChartJS, defaults } from "chart.js/auto";
-// import { Bar, Doughnut, Line } from "react-chartjs-2";
-// import BarData from "./BarData.js"
-// import {BarCharts} from"./BarCharts.module.css"
-// defaults.maintainAspectRatio = false;
-// defaults.responsive = true;
-
-// defaults.plugins.title.display = true;
-// defaults.plugins.title.align = "start";
-// defaults.plugins.title.font.size = 20;
-// defaults.plugins.title.color = "black";
-
-
-// function BarChart()  {
-//     return(
-        
-//             <div className="App">
-//               <div className="dataCard revenueCard">
-//                 <Line
-//                   data={{
-//                     labels: BarData.map((data) => data.label),
-//                     datasets: [
-//                       {
-//                         label: "Revenue",
-//                         data: BarData.map((data) => data.revenue),
-//                         backgroundColor: "#064FF0",
-//                         borderColor: "#064FF0",
-//                       },
-//                       {
-//                         label: "Cost",
-//                         data: BarData.map((data) => data.cost),
-//                         backgroundColor: "#FF3030",
-//                         borderColor: "#FF3030",
-//                       },
-//                     ],
-//                   }}
-//                   options={{
-//                     elements: {
-//                       line: {
-//                         tension: 0.5,
-//                       },
-//                     },
-//                     plugins: {
-//                       title: {
-//                         text: "Monthly Revenue & Cost",
-//                       },
-//                     },
-//                   }}
-//                 />
-//               </div>
-//               </div>
-//     )
-// }
-
-// export default BarChart;
-
 import React from "react";
 import { Chart as ChartJS, defaults } from "chart.js/auto";
 import { Bar, Doughnut, Line } from "react-chartjs-2";
 import BarCharts from "./BarCharts.module.css"
-
-
-import BarData from "./BarData.js";
-import SourceData from "./SourceData.js";
+import axios from "axios"
+import { useEffect, useState } from "react";
 
 
 defaults.maintainAspectRatio = false;
@@ -89,6 +15,122 @@ defaults.plugins.title.font.size = 20;
 defaults.plugins.title.color = "black";
 
 const BarChart = () => {
+
+
+  //fetch Line
+  const [orderss , setOrderss] = useState([])
+  useEffect(()=>{
+    const fetchOrders = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/order/all');
+        console.log('Response:', response);
+         setOrderss(response.data.orders);
+      } catch (error) {
+        throw new Error('Error fetching data');
+      }
+    };
+    fetchOrders()
+  },[])
+
+  // Group orders by month
+const ordersByMonth = orderss.reduce((acc, order) => {
+  const month = new Date(order.createdAt).getMonth();
+  if (!acc[month]) {
+    acc[month] = [];
+  }
+  acc[month].push(order);
+  return acc;
+}, {});
+
+// Calculate total price for each month
+const monthlyTotals = Object.values(ordersByMonth).map((ordersInMonth) =>
+  ordersInMonth.reduce((total, order) => total + order.totalPrice, 0)
+);
+
+
+
+
+
+
+//fetch Bar
+const [productsArray, setProductsArray] = useState([])
+useEffect(() => {
+const fetchProducts = async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/product/getall');
+    console.log('Response:', response);
+    setProductsArray(response.data.products);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    throw new Error('Error fetching products');
+  }
+};
+fetchProducts()
+}, [])
+
+ // Preprocess data for the bar chart
+ const categoryCounts = productsArray.reduce((acc, product) => {
+  const categoryName = product.categoryID.arabicName;
+
+  // Increment count for the category or set it to 1 if not present
+  acc[categoryName] = (acc[categoryName] || 0) + 1;
+
+  return acc;
+}, {});
+
+const chartData = {
+  labels: Object.keys(categoryCounts),
+  datasets: [
+    {
+      label: 'Count',
+      data: Object.values(categoryCounts),
+      backgroundColor: ['#368681', '#F1CAB1', '#4D342B'],
+      hoverBackgroundColor: ['#368681', '#F1CAB1', '#4D342B'],
+
+      borderRadius: 7,
+    },
+  ],
+};
+
+const chartOptions = {
+  plugins: {
+    title: {
+      text: 'Products in Category',
+    },
+  },
+};
+
+
+
+//fetch pie
+  const subcategoryDistribution = productsArray.reduce((acc, product) => {
+    const subcategory = product.subCategoryID;
+    if (subcategory && subcategory.arabicName) {
+      const subcategoryName = subcategory.arabicName;
+      // Increment count for the subcategory or set it to 1 if not present
+      acc[subcategoryName] = (acc[subcategoryName] || 0) + 1;
+    }
+    return acc;
+  }, {});
+const pieChartData = {
+  labels: Object.keys(subcategoryDistribution),
+  datasets: [
+    {
+      data: Object.values(subcategoryDistribution),
+      backgroundColor: ['#368681', '#F1CAB1', '#4D342B'], 
+      hoverBackgroundColor: ['#368681', '#F1CAB1', '#4D342B'],
+    },
+  ],
+};
+const pieChartOptions = {
+  plugins: {
+    title: {
+      text: 'Products in SubCategory',
+    },
+  },
+};
+
+
   return (
     <div className={BarCharts.App}>
  <div className={`${BarCharts.dataCard} ${BarCharts.revenueCard}`}>
@@ -127,28 +169,7 @@ const BarChart = () => {
 
       <div className={`${BarCharts.dataCard} ${BarCharts.customerCard}`}>
         <Bar
-          data={{
-            labels: SourceData.map((data) => data.label),
-            datasets: [
-              {
-                label: "Count",
-                data: SourceData.map((data) => data.value),
-                backgroundColor: [
-                  '#368681',
-                  "#F1CAB1",
-                  "#4D342B",
-                ],
-                borderRadius: 7,
-              },
-            ],
-          }}
-          options={{
-            plugins: {
-              title: {
-                text: "Products",
-              },
-            },
-          }}
+          data={chartData} options={chartOptions}
         />
       </div>
 
@@ -156,32 +177,8 @@ const BarChart = () => {
 
       <div className={`${BarCharts.dataCard} ${BarCharts.categoryCard}`}>
         <Doughnut
-          data={{
-            labels: SourceData.map((data) => data.label),
-            datasets: [
-              {
-                label: "Count",
-                data: SourceData.map((data) => data.value),
-                backgroundColor: [
-                  '#368681',
-                  "#F1CAB1",
-                  "#4D342B",
-                ],
-                borderColor: [
-                  '#368681',
-                  "#F1CAB1",
-                  "#4D342B",
-                ],
-              },
-            ],
-          }}
-          options={{
-            plugins: {
-              title: {
-                text: "Orders",
-              },
-            },
-          }}
+          data={pieChartData} options={pieChartOptions}
+          
         />
       </div>
     </div>
