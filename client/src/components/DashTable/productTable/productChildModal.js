8127a9cf87fd18eb7bde17308fca43b64f49770f
Yahya Nashar <141627413+YahyaNashar22@users.noreply.github.com>
2024-02-02 13:32:50 +0200

    Update productChildModal.js

--- client/src/components/DashTable/productTable/productChildModal.js
+++ client/src/components/DashTable/productTable/productChildModal.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
-import ProductForm from "../../../components/ProducForm /ProductForm";
+import ProductForm from "../../../components/ProducForm/ProductForm";
 import axios from "axios";
 import { toast } from "react-toastify";
 
